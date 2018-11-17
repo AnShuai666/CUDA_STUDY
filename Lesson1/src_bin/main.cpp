b0c49a3fc9810e703fa399f6b88595255874695a
yft1996 <yangfengtuo@163.com>
2018-11-17 18:49:15 +0800

    change file main.cpp

--- Lesson1/src_bin/main.cpp
+++ Lesson1/src_bin/main.cpp
@@ -1,7 +1,24 @@
-#include "rgb2gray.cuh"
+#include "gray.cuh"
+#include "image_io.h"
 
-int main()
+//extern "C" 
+//void func();
+int main(int argc,char * argv[])
 {
-	func();
-return 0;
+  imageio::Imageio pine;
+  pine.pathRead("/home/anshuai/yft/November/error_gray/res/pine.jpg");
+  pine.imageRead();
+  pine.imageShow(pine.image_BGR);
+  pine.preprocess();
+  pine.imageShow(pine.image_RGBA);
+
+  pine.createGrayimage();
+
+  pine.image_GRAY_out = cudafunc(pine.image_RGBA,pine.image_GRAY_in);
+
+  pine.pathWrite("/home/anshuai/yft/November/error_gray/output");
+  pine.postprocess("graypine.jpg");
+  pine.imageShow(pine.image_GRAY_out);
+  return 0;
 }
+
