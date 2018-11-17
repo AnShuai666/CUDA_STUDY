638e088475f683476b7d49a0b2a23861486a578c
yft1996 <yangfengtuo@163.com>
2018-11-17 18:42:37 +0800

    add file define.h

---
+++ Lesson1/include/include_cxx/define.h
@@ -0,0 +1,11 @@
+//
+// Created by anshuai on 18-11-17.
+//
+
+#ifndef GRAY_DEFINE_H
+#define GRAY_DEFINE_H
+
+#define IMAGEIO_NAMESPACE_BEGIN namespace imageio {
+#define IMAGEIO_NAMESPACE_END }
+
+#endif //GRAY_DEFINE_H
